7e8755ec361b4aa36ddc6f7779e44d7a99045955
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-20 12:00:31 +0300

    Geocode url log

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,3 +7,10 @@ request({
 }, (error, response) => {
   console.log(`${response.body.daily.data[0].summary} Its is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain`)
 })
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicnVzYWxleCIsImEiOiJjazZ1aGZuaDIwNTN2M2VvYWVrbW45eTd2In0.k187H5s4Bvd2lfiZMUqCtg"
+request({
+  url: geocodeURL,
+  json: true
+}, (error, response) => {
+console.log(`latigude:${response.body.features[0].center[0]}, longitude:${response.body.features[0].center[1]}`);
+})
