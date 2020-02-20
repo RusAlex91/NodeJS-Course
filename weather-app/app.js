837c03c76763e2d64404c934d6e780742aaf8584
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-20 12:40:38 +0300

    error handlers

--- weather-app/app.js
+++ weather-app/app.js
@@ -5,12 +5,25 @@ request({
   url: url,
   json: true
 }, (error, response) => {
-  console.log(`${response.body.daily.data[0].summary} Its is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain`)
+  if (error) {
+    console.log("Unable to connect, check internet connection");
+  } else if (response.body.error) {
+console.log("Unable to find location");
+  } else {
+    console.log(`${response.body.daily.data[0].summary} Its is currently ${response.body.currently.temperature} degrees out. There is a ${response.body.currently.precipProbability}% chance of rain`)
+  }
+
 })
-const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1IjoicnVzYWxleCIsImEiOiJjazZ1aGZuaDIwNTN2M2VvYWVrbW45eTd2In0.k187H5s4Bvd2lfiZMUqCtg"
+const geocodeURL = "https://api.mapbox.com/geocoding/v5/mapbox.places/Lossssss%20Angeles.json?access_token=pk.eyJ1IjoicnVzYWxleCIsImEiOiJjazZ1aGZuaDIwNTN2M2VvYWVrbW45eTd2In0.k187H5s4Bvd2lfiZMUqCtg"
 request({
   url: geocodeURL,
   json: true
 }, (error, response) => {
+  if (error){
+    console.log("Unable to connect to location service");
+  } else if (response.body.features.length === 0){
+console.log("location not found");
+  } else {
 console.log(`latigude:${response.body.features[0].center[0]}, longitude:${response.body.features[0].center[1]}`);
+  }
 })
