0b4eb96d19ecfa9d0914c0ed57ff10ac3158ff54
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-20 14:11:54 +0300

    utils foulder and geocode.js create

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,5 +1,5 @@
 const request = require("request");
-
+const geocode = require("./utils/geocode")
 // const url = "https://api.darksky.net/forecast/8a0045d572520f63c6299049baafdfb2/37.8267,-122.4233?units=us"
 // request({
 //   url: url,
@@ -28,27 +28,9 @@ const request = require("request");
 //   }
 // })
 
-const geocode = (address, callback) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicnVzYWxleCIsImEiOiJjazZ1aGZuaDIwNTN2M2VvYWVrbW45eTd2In0.k187H5s4Bvd2lfiZMUqCtg`
-  request({
-    url: url,
-    json: true
-  }, (error, response) => {
-    if (error) {
-      callback("Unable to connect to location services!", undefined)
-    } else if (response.body.features.length === 0) {
-      callback("Unable to find location. Try another search.",undefined)
-    } else {
-      callback(undefined, {
-        latigude: response.body.features[0].center[0],
-        longitude: response.body.features[0].center[1],
-        location: response.body.features[0].place_name
-      })
-    }
-  })
-}
 
-geocode("New York", (error, data) => {
+
+geocode("Boston", (error, data) => {
 console.log("Error", error);
 console.log("Data", data);
 })
---
+++ weather-app/utils/geocode.js
@@ -0,0 +1,21 @@
+const request = require("request")
+const geocode = (address, callback) => {
+  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${address}.json?access_token=pk.eyJ1IjoicnVzYWxleCIsImEiOiJjazZ1aGZuaDIwNTN2M2VvYWVrbW45eTd2In0.k187H5s4Bvd2lfiZMUqCtg`
+  request({
+    url: url,
+    json: true
+  }, (error, response) => {
+    if (error) {
+      callback("Unable to connect to location services!", undefined)
+    } else if (response.body.features.length === 0) {
+      callback("Unable to find location. Try another search.",undefined)
+    } else {
+      callback(undefined, {
+        latigude: response.body.features[0].center[0],
+        longitude: response.body.features[0].center[1],
+        location: response.body.features[0].place_name
+      })
+    }
+  })
+}
+module.exports = geocode
