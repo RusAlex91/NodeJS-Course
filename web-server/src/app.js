b65c826c0fdae17dfd3e7c86fee4e29be97dad1f
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-25 11:56:47 +0300

    create index.html, fix app.js

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,13 +1,14 @@
+const path = require("path")
 const express = require("express")
 
 const app = express()
+const publicDirPath = path.join(__dirname,"../public")
 
-app.get("", (req, res) => {
-  res.send("hello express")
-})
+app.use(express.static(publicDirPath))
 
 app.get("/help", (req, res)=> {
-  res.send()
+  res.send("call cops for help")
+})
 
 app.get("/about", (req, res)=> {
   res.send("about page")
