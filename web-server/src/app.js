ca172319059b7970d2afd9219ec9fa08801a7eda
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-25 14:11:59 +0300

    hbs index, delite index.html

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,18 @@
-
 const path = require("path")
 const express = require("express")
-
+debugger
 const app = express()
 const publicDirPath = path.join(__dirname,"../public")
 
+app.set("view engine", "hbs")
 app.use(express.static(publicDirPath))
+
+app.get("",(req, res) => {
+  res.render("index", {
+    title: "Weather App",
+    name: "Lox"
+  })
+})
 
 app.get("/weather", (req, res)=> {
   res.send("<h1>Weather")
