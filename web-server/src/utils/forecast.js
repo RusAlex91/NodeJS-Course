b82369535dc33c5f994f3c646a46df564901c996
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-26 12:54:55 +0300

    continue course

---
+++ web-server/src/utils/forecast.js
@@ -0,0 +1,18 @@
+const request = require("request")
+const forecast = (latitude, longitude, callback) => {
+
+  const url = `https://api.darksky.net/forecast/8a0045d572520f63c6299049baafdfb2/${latitude},${longitude}`
+  request({
+    url,
+    json: true
+  }, (error, {body}) => {
+    if (error) {
+        callback('Unable to connect to weather service!', undefined)
+    } else if (body.error) {
+        callback('Unable to find location', undefined)
+    } else {
+      callback(undefined, `${body.daily.data[0].summary} Its is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`)
+    }
+  })
+}
+module.exports = forecast
