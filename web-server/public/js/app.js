c24f57b3c65dddd9a29cd188a9c8258d2efc91ed
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-26 15:05:33 +0300

    finish weather up (base)

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -11,6 +11,10 @@ console.log(data);
 
 const weatherForm = document.querySelector("form")
 const search = document.querySelector("input")
+const messageOne = document.querySelector("#message-1")
+const messageTwo = document.querySelector("#message-2")
+
+
 
 
 weatherForm.addEventListener("submit", (e) => {
@@ -21,9 +25,10 @@ const location = search.value
 fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
   response.json().then((data)=> {
 if(data.error) {
-  console.log(data.error);
+  messageTwo.textContent = data.error
 } else {
-  console.log(data);
+  messageOne.textContent = data.location
+  messageTwo.textContent = data.forecast
 }
   })
 })
