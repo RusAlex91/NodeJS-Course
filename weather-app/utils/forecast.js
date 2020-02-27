7d4faf7b18b0fef72669e8f749b8cb8df454ad2c
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-27 14:23:45 +0300

    add sunset and sunrise

--- weather-app/utils/forecast.js
+++ weather-app/utils/forecast.js
@@ -11,8 +11,27 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
         callback('Unable to find location', undefined)
     } else {
-      callback(undefined, `${body.daily.data[0].summary} Its is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain`)
+      callback(undefined, `${body.daily.data[0].summary} Its is currently ${body.currently.temperature} degrees out. There is a ${body.currently.precipProbability}% chance of rain. Sunrise at ${formattedTime(body.daily.data[0].sunriseTime)}. Sunset at ${formattedTime(body.daily.data[0].sunsetTime)}.`)
     }
   })
 }
 module.exports = forecast
+
+function formattedTime(time) {
+let unix_timestamp = time
+// Create a new JavaScript Date object based on the timestamp
+// multiplied by 1000 so that the argument is in milliseconds, not seconds.
+var date = new Date(unix_timestamp * 1000);
+// Hours part from the timestamp
+var hours = date.getHours();
+// Minutes part from the timestamp
+var minutes = "0" + date.getMinutes();
+// Seconds part from the timestamp
+var seconds = "0" + date.getSeconds();
+
+// Will display time in 10:30:23 format
+var formattedTimeResult = hours + ':' + minutes.substr(-2) + ':' + seconds.substr(-2);
+console.log(formattedTimeResult);
+}
+
+formattedTime(body.daily.data[0].sunriseTime)
