f10adcb5ca5ee27bf6adea9e2365dfbc267679b2
RusAlex91 <aocrusalex91@yandex.ru>
2020-02-27 13:37:24 +0300

    hiroku setup

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,8 +1,8 @@
 console.log("client side loaded");
 
 fetch("http://puzzle.mead.io/puzzle").then((response) => {
-  response.json().then((data)=> {
-console.log(data);
+  response.json().then((data) => {
+    console.log(data);
   })
 })
 
@@ -18,18 +18,18 @@ const messageTwo = document.querySelector("#message-2")
 
 
 weatherForm.addEventListener("submit", (e) => {
-e.preventDefault()
+  e.preventDefault()
 
-const location = search.value
+  const location = search.value
 
-fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
-  response.json().then((data)=> {
-if(data.error) {
-  messageTwo.textContent = data.error
-} else {
-  messageOne.textContent = data.location
-  messageTwo.textContent = data.forecast
-}
+  fetch(`/weather?address=${location}`).then((response) => {
+    response.json().then((data) => {
+      if (data.error) {
+        messageTwo.textContent = data.error
+      } else {
+        messageOne.textContent = data.location
+        messageTwo.textContent = data.forecast
+      }
+    })
   })
-})
 })
